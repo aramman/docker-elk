5bff842fee21676b6b78d4be2f5b5cec55171721
root <aram.manvelyan@gmail.com>
2023-04-28 05:36:20 -0500

    scripts

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -18,6 +18,7 @@ users_passwords=(
 	[heartbeat_internal]="${HEARTBEAT_INTERNAL_PASSWORD:-}"
 	[monitoring_internal]="${MONITORING_INTERNAL_PASSWORD:-}"
 	[beats_system]="${BEATS_SYSTEM_PASSWORD=:-}"
+	[readall]="${READALL_USER_PASSWORD=:-}"
 )
 
 declare -A users_roles
@@ -27,6 +28,7 @@ users_roles=(
 	[filebeat_internal]='filebeat_writer'
 	[heartbeat_internal]='heartbeat_writer'
 	[monitoring_internal]='remote_monitoring_collector'
+	[readall]='readall'
 )
 
 # --------------------------------------------------------
@@ -38,6 +40,7 @@ roles_files=(
 	[metricbeat_writer]='metricbeat_writer.json'
 	[filebeat_writer]='filebeat_writer.json'
 	[heartbeat_writer]='heartbeat_writer.json'
+	[readall]='readall.json'
 )
 
 # --------------------------------------------------------
